b256970fd6b30d37e10428a21ec31c260d8403cd
nojasne <poliak.dano@gmail.com>
2019-10-14 15:00:27 +0200

    missing react table styles

--- src/components/common/DictionaryTable.js
+++ src/components/common/DictionaryTable.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactTable from 'react-table';
+import 'react-table/react-table.css';
 import Button from './Button';
 import Input from './Input';
 import { getPageSizeRoundToFive } from '../../utils';
